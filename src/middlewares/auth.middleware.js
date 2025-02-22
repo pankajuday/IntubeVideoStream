4e5b7b9b58d11433baea2df6354de07babd90e3c
pankaj <pankajuday7033@gmail.com>
2025-02-23 01:47:45 +0530

    updated auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     try {
-        const token = req.cookies?.accessToken ;
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "").trim();
 
         if (!token) throw new ApiError(401, "Unauthorized request");
 
