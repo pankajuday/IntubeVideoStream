e9e9893b1e76c50516f81177a53120957a2eb79d
pankaj <pankajuday7033@gmail.com>
2025-07-09 08:23:30 +0530

    feat: add username field to video aggregation for enhanced user information

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -100,6 +100,7 @@ const getAllVideos = asyncHandler(async (req, res) => {
                         $project: {
                             fullName: 1,
                             avatar: 1,
+                            username:1,
                         },
                     },
                 ],
@@ -113,6 +114,9 @@ const getAllVideos = asyncHandler(async (req, res) => {
                 avatar: {
                     $arrayElemAt: ["$owner.avatar", 0],
                 },
+                username:{
+                    $arrayElemAt:["$owner.username",0]
+                }
             },
         },
     ]);
